cada92c923d134cac6e63abcaf92c02f6458fbcf
User <ruskoso36@gmail.com>
2025-06-18 09:18:56 +0600

    Add sd

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,9 +1,9 @@
 import { io } from "socket.io-client";
 
-// ❌ Было:
-const socket = io("http://localhost:5000");
 
-// ✅ Стало:
-//const socket = io("http://192.168.88.147:5000");
+const socket = io(import.meta.env.VITE_BACKEND_URL);
+
+
+
 
 export default socket;
